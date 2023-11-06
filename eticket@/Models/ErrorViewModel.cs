ddd004d7060bc816b210ec2a7f8de3de2a617069
Harika <harikagadaraju>
2023-11-06 15:52:23 +0530

    Add project files.

---
+++ eticket@/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eticket_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
